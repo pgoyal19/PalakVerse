b5fbcd324cb427595027c53a4f6762338e2193c0
Palak Goyal <palakgoyal0119@gmail.com>
2026-01-17 14:08:34 +0530

    Add GitHub Pages deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/PalakVerse/',
   resolve: {
     dedupe: ['react', 'react-dom'],
   },
